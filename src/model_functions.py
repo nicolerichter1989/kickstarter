036896e03a3cf43a2e80978efe3bf73573ac74b1
NICOLERICHTER1989 <NICOLERICHTER1989@GMAIL.COM>
2021-11-12 23:06:53 +0100

    submit 12112021

--- src/model_functions.py
+++ src/model_functions.py
@@ -3,100 +3,115 @@
 import pandas as pd
 import numpy as np
 from imblearn.over_sampling import SMOTE
-
+from imblearn.under_sampling import TomekLinks
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, confusion_matrix
-
-
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+import math
+from sklearn.metrics import mean_squared_error
 
 ## functions
 
-def classification_tree():
+def classification_tree(X, y):
 
     '''write description'''
 
     #what needs to be the input?
     #what needs to be the return?
 
+    # split in train and test data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    # pick and fit model
     model = DecisionTreeClassifier()
     model.fit(X_train, y_train)
 
-    return
+    # make predictions
+    predictions = model.predict(X_test)
+    MAE = mean_absolute_error(y_test, predictions)
+    MSE = mean_squared_error(y_test, predictions)   
+    RSME = math.sqrt(MSE)
+
+    return print('MAE' + MAE, 'MSE' +  MSE, 'RSME' +  RSME)
 #
 #
 #
-def classification_KNN():
+def classification_KNN(X, y):
 
     '''write description'''
 
     #what needs to be the input?
     #what needs to be the return?
 
+    # split in train and test data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    # pick and fit model  
     model = KNeighborsRegressor(n_neighbors=4)
     model.fit(X_train, y_train)
 
-    return
+    # make predictions
+    predictions = model.predict(X_test)
+    MAE = mean_absolute_error(y_test, predictions)
+    MSE = mean_squared_error(y_test, predictions)   
+    RSME = math.sqrt(MSE)
+
+    return print('MAE' + MAE, 'MSE' +  MSE, 'RSME' +  RSME)
 #
 #
 #
-def classification_LogisticRegression():
+#
+def classification_LogisticRegression(X, y):
 
     '''write description'''
 
-    return
+    #what needs to be the input?
+    #what needs to be the return?
+
+    # split in train and test data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+    # pick and fit model  
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+ 
+    # make predictions
+    predictions = model.predict(X_test)
+    MAE = mean_absolute_error(y_test, predictions)
+    MSE = mean_squared_error(y_test, predictions)   
+    RSME = math.sqrt(MSE)
+
+    return print('MAE' + MAE, 'MSE' +  MSE, 'RSME' +  RSME)
 #
 #
 #
-
-
-
-
-def x_y_split(target, df):
+def confusion_matrix(y_test, predictions):
 
     '''write description'''
 
-    y = df[target]
-    X = df.drop(target, axis=1)
+    #what needs to be the input?
+    #what needs to be the return?
 
-    return df
-#
-#
-#
-def train_test_split(model):
+    cf_matrix = confusion_matrix(y_test, predictions)
+    group_names = ['True A', 'False A','False B', 'True B']
 
-    '''write description'''
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    classification = LogisticRegression(random_state=42, max_iter=1000)
-    classification.fit(X_train, y_train)
-
-    return x
-#
-#
-#
-def confusion_matrix():
-
-    '''write description'''
-
-    group_names = ['True failed', 'False failed', 'False success', 'True success']
     group_counts = ["{0:0.0f}".format(value) for value in cf_matrix.flatten()]
     group_percentages = ["{0:.2%}".format(value) for value in cf_matrix.flatten()/np.sum(cf_matrix)]
     labels = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in zip(group_names,group_counts,group_percentages)]
     labels = np.asarray(labels).reshape(2,2)
-    
-    return sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Blues')
+    sns.heatmap(cf_matrix, annot=labels, fmt='', cmap='Blues')
+
+    return
 #
 #
 #
 
 
 
-### write functions to be able to easier run all different model I want to try
 
 
